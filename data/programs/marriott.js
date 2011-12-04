c1dd0084e457b09b65d1c12cff0161c2d3314d27
Shane Carveo <mixedpuppy@gmail.com>
2011-12-04 12:17:16 -0800

    add marriott

---
+++ data/programs/marriott.js
@@ -0,0 +1,59 @@
+
+// 0=,8=153470646,41=US,22=Logged in ,9=Basic
+memnum_re = /8=(\d+),/;
+// Balance: 5,139 points
+balance_re = /Balance:\s+([\d,]*)\s+points/;
+nights_re = /8=(\d+),/;
+
+self.port.on('program', function(account) {
+  //console.log('marriott data '+JSON.stringify(account));
+  if (unsafeWindow.location == 'https://www.marriott.com/signInError.mi') {
+    self.port.emit('loginFailure');
+    return;
+  }
+  if (document.signInForm) {
+    if (!account.username) {
+      var memberNumber = $('input[name="visibleUserName"]').val();
+      dump("no account, memberNumber is "+memberNumber+"\n");
+      if (!memberNumber) {
+        self.port.emit('loginFailure');
+        return;
+      }
+    }
+    if (account.username) {
+      $('input[name="visibleUserName"]').val(account.username);
+    }
+    dump("no account, password is "+account.password+"\n");
+    $('input[name="j_password"]').val(account.password);
+    
+    $('button[name="btnSubmit"]').click();
+    //document.signInForm.submit();
+    //unsafeWindow.logIn();
+    return;
+  }
+  if (unsafeWindow.location == 'https://www.marriott.com/default.mi') {
+    // move ourselves to the page we want
+    unsafeWindow.location = 'https://www.marriott.com/rewards/myAccount/activity.mi';
+    return;
+  }
+  
+  var fullName = $("#account-name > a").text().trim();
+  var memberNumber = memnum_re.exec($("#omniEvars").val())[1];
+  var other = $("#account-information li");
+  var accountBalance = balance_re.exec($(other[3]).text().trim())[1];
+  var memberLevel = $(other[0]).text().trim();
+
+  let data = {
+    name: fullName,
+    account: memberNumber,
+    balance: accountBalance,
+    status: memberLevel,
+    statusMiles: "",
+    expiration: ""
+  }
+  
+  self.port.emit('data', data);
+});
+
+console.log("marriott pageMod loaded");
+self.port.emit('ready');
