ef257a9185bf4d17170ec0335b011275d680c543
Shane Carveo <mixedpuppy@gmail.com>
2011-12-04 09:11:37 -0800

    add starwood

---
+++ data/programs/starwood.js
@@ -0,0 +1,33 @@
+
+info_re = /(.*) \d+/;
+
+self.port.on('program', function(account) {
+  //console.log('starwood data '+JSON.stringify(account));
+  if (document.stdloginForm) {
+    var username = $('input[name="login"]');
+    username.value = account.username;
+    var passwordField = $('input[name="password"]');
+    passwordField.value = account.password;
+    document.stdloginForm.submit();
+    return;
+  }
+  
+  var fullName = $("div.fullName").text().trim();
+  var memberNumber = $("div.memberNumber > b").text().trim();
+  var accountBalance = $("#accountBalance > b").text().trim();
+  var memberLevel = info_re.exec($("div.memberLevelCopy").text().trim())[1];
+
+  let data = {
+    name: fullName,
+    account: memberNumber,
+    balance: accountBalance,
+    status: memberLevel,
+    statusMiles: "",
+    expiration: ""
+  }
+  
+  self.port.emit('data', data);
+});
+
+console.log("starwood pageMod loaded");
+self.port.emit('ready');
